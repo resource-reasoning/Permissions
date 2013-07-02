904b476f2f9ce326d5d05bffb0ca8a91083e6d63
Thomas Dinsdale-Young <thomas-github@d-y.me.uk>
2013-07-02 17:58:29 +0100

    Some progress on Coq

--- PermissionLogic.v
+++ PermissionLogic.v
@@ -152,7 +152,41 @@ Module Type InhabitationGrid.
       In f (extensions g) /\ f' == eval f.
 End InhabitationGrid.
 
+Fixpoint mix_with {A B C} (f : A -> B -> C) (la : list A) (lb : list B) : list C :=
+  match la with
+  | nil => nil
+  | cons x xs => (map (f x) lb) ++ mix_with f xs lb
+  end.
+
+Lemma In_mix_with {A B C} (f : A -> B -> C) la lb :
+  forall z, In z (mix_with f la lb) <-> exists x, exists y, In x la /\ In y lb /\ z = f x y.
+ induction la.
+  simpl; firstorder.
+  simpl; intuition.
+   apply in_app_or in H; destruct H.
+    clear IHla.
+    induction lb.
+     contradiction H.
+     destruct H.
+      subst; exists a; exists a0; intuition.
+      apply IHlb in H; destruct H; destruct H.
+      intuition; subst.
+       exists x; exists x0; intuition.
+       exists x; exists x0; intuition.
+   firstorder.
+
+   firstorder; subst.
+   remember lb as lb'.
+   rewrite Heqlb' at 2; clear Heqlb'.
+   clear IHla; induction lb'.
+    contradiction H0.
+    firstorder; subst.
+     left; reflexivity.
+Qed.
+
+
 Module TreeInhabitationGrid.
+  Import InhabitationFunctionDefs.
   Inductive et : nat -> Type :=
     | et_some : et 0
     | et_none : forall n, et n
@@ -182,7 +216,82 @@ Module TreeInhabitationGrid.
   Definition eval n (g : grid n) (v : vector bool n) :=
     eval' g (vreverse v).
   Definition empty_grid := et_some.
+  Proposition empty_grid_is_empty : forall v, eval empty_grid v = true.
+   firstorder.
+  Qed.
+
+
+  Fixpoint extensions n (g : grid n) : list (grid (S n)) :=
+  match g with
+  | et_some => et_branch et_some et_some :: et_branch et_some (et_none 0) :: et_branch (et_none 0) et_some :: nil
+  | et_none m => et_none (S m) :: nil
+  | et_branch m l r => mix_with (@et_branch (S m) ) (extensions l) (extensions r)
+  end.
+
+(*  Compute (extensions (et_branch (et_branch et_some et_some) (et_branch (et_none _) (et_some)))). *)
   
+  Lemma eval_branch n (g1 g2 : grid n) v : eval' (et_branch g1 g1) (vcons true v) = eval' g1 v.
+  reflexivity.
+  Qed.
+  
+
+  Proposition extensions_are_extensions : forall n (f : grid (S n)) (g : grid n),
+    In f (extensions g) -> is_extension_of (eval f) (eval g).
+   induction g; intros.
+    simpl in H; intuition; subst; intro;
+     refine (match v in (vector _ n) with vnil => _ | vcons _ _ _ => id end);
+     reflexivity.
+
+    simpl in H; intuition; subst.
+     intro.
+     induction v.
+      reflexivity.
+      reflexivity.
+
+   simpl in H.
+   apply In_mix_with in H.
+   destruct H; destruct H; intuition; subst.
+   apply IHg1 in H0.
+   apply IHg2 in H.
+   intro.
+   clear -H0 H.
+   induction n.
+    
+    refine (match v in (vector _ n) with vnil => id | vcons _ _ _ => _ end).
+
+
+   refine (match v with vnil => _ | vcons _ _ _ => _ end _).
+   unfold eval.
+   unfold eval'.
+   simpl.
+   refine (match v in (vector _ m) with vnil => id | vcons _ _ _ => _ end).
+
+   induction (extensions g1).
+    contradiction H.
+
+     reflexivity.
+
+
+      ID
+     refine (match v with _ => _ end).
+     set (n := 0).
+     destruct v.
+     change (vector bool n) in v.
+     assert (n = 0) by reflexivity.
+     revert n v H.
+     induction v.
+     intro.
+    subst.
+
+    induc
+   inversion H.
+   induction H.
+   intros.
+   simpl in H.
+
+  Axiom all_extensions : forall n (g : grid n) (f' : inhf (S n)), 
+    is_extension_of f' (eval g) -> exists f,
+      In f (extensions g) /\ f' == eval f.
 
 
 Module Type InhabitationGridSpec (Import ig : InhabitationGrid).
